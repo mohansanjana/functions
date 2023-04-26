469c5bbfb5ecc1e75b4f1f656cab31719f8f765d
mohansanjana <mohas206@newschool.edu>
2023-04-25 22:46:28 -0400

    Update EK203.js

--- assets/EK203.js
+++ assets/EK203.js
@@ -7,7 +7,7 @@
  img.addEventListener('click', function(){
    toggle = !toggle;
    if(toggle){
-       img.src = 'img\\EK203.PNG';
+       img.src = 'img\\EK203.png';
    }else{
        img.src = 'img\\EK203.gif';
    }
